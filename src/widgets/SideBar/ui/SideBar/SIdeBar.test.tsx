d776de46b139a43116c3ff87555681c6708790dd
GrigoriyLomtev <grisha2198@gmail.com>
2024-04-24 20:18:02 +0300

    fix tests

--- src/widgets/SideBar/ui/SideBar/SIdeBar.test.tsx
+++ src/widgets/SideBar/ui/SideBar/SIdeBar.test.tsx
@@ -2,15 +2,16 @@ import { fireEvent, screen } from '@testing-library/react';
 import {
   renderWithTransalation,
 } from 'shared/lib/tests/renderWithTransalation/renderWithTransalation';
+import { ComponentRender } from 'shared/lib/tests/ComponentRender/ComponentRender';
 import { SideBar } from './SideBar';
 
 describe('classNames', () => {
   test('with only first param', () => {
-    renderWithTransalation(<SideBar />);
+    ComponentRender(<SideBar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
   test('test toggle', () => {
-    renderWithTransalation(<SideBar />);
+    ComponentRender(<SideBar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
