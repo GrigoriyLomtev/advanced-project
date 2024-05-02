8d73632116ab00da416e204b3f397c51751d00dd
GrigoriyLomtev <grisha2198@gmail.com>
2024-05-02 10:41:54 +0300

    add entity user, ui input, feature login form

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,17 +1,14 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
-import styles from './LoginModal.module.scss';
 import { LoginForm } from '../LoginForm/LoginForm';
 
 interface LoginModalProps {
-  className?: string
   isOpen: boolean
   onClose: () => void
 }
 
 export function LoginModal(props: LoginModalProps) {
   const {
-    className, isOpen, onClose,
+    isOpen, onClose,
   } = props;
 
   return (
@@ -19,7 +16,6 @@ export function LoginModal(props: LoginModalProps) {
       lazy
       isOpen={isOpen}
       onClose={onClose}
-      className={classNames(styles.block, {}, [className])}
     >
       <LoginForm />
     </Modal>
