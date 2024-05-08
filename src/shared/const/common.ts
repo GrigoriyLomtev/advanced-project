0b7ac9d220dcc01f5da4679ad75995860a0fcec2
GrigoriyLomtev <grisha2198@gmail.com>
2024-05-08 22:04:08 +0300

    cleaning

--- src/shared/const/common.ts
+++ src/shared/const/common.ts
@@ -1,14 +1,14 @@
 export enum Currency {
-  'RUB' = 'RUB',
-  'EUR' = 'EUR',
-  'USD' = 'USD'
+  RUB = 'RUB',
+  EUR = 'EUR',
+  USD = 'USD',
 }
 export enum Country {
-  'Russia' = 'Russia',
-  'England' = 'England',
-  'USA' = 'USA',
-  'Germany' = 'Germany',
-  'France' = 'France',
-  'UAE' = 'UAE',
-  'Brazil' = 'Brazil',
+  RUSSIA = 'Russia',
+  ENGLAND = 'England',
+  USA = 'USA',
+  GERMANY = 'Germany',
+  FRANCE = 'France',
+  UAE = 'UAE',
+  BRAZIL = 'Brazil',
 }
